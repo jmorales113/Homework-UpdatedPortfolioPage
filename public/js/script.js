34669886a79b190882ad8d7d0540de226671fd18
jmorales113 <jmorales113@gmail.com>
2020-12-04 22:30:23 -0800

    converted jquery to vanilla javascript and refactored

---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+const $menuToggler = document.querySelector(".menu-toggler")
+const $topNav = document.querySelector(".top-nav")
+const $navLink = document.querySelector(".top-nav, .nav-link")
+
+$menuToggler.addEventListener("click", () => {
+  $menuToggler.classList.toggle("open")
+  $topNav.classList.toggle("open")
+})
+
+$navLink.addEventListener("click", () => {
+  $menuToggler.classList.remove("open")
+  $topNav.classList.remove("open")
+})
+
+AOS.init({
+  easing: "ease",
+  duration: 1800,
+});
+
+
